33653c1040eed12a38764f682a21c209e2c91bd5
Dwikso <quentin.pellosse2004@gmail.com>
2024-11-13 20:46:39 +0100

    feat(ajout de l'image de fond): ajout de l'image de fond

--- js/view.js
+++ js/view.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const plateau = document.getElementById('plateau');
-    const nb_ligne = 5; // nombre de lignes
-    const nb_col = 5;   // nombre de colonnes
+
+    const nb_ligne = 5;
+    const nb_col = 5;
 
     for (let i = 0; i < nb_ligne; i++) {
         for (let j = 0; j < nb_col; j++) {
